63528dc115fa3ee24cb6b711d80f9669217685b8
adrianopereira0525-glitch <adrianopereira0525@gmail.com>
2026-06-20 13:34:32 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MAJSSite from './MAJSSite.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MAJSSite />
+  </React.StrictMode>,
+)
